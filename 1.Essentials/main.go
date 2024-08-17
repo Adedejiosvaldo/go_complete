37c18dfea941ae7b601a07b8eaa4ee9971c8c779
adedejiosvaldo@ <adedejiosvaldo@gmail.com>
2024-08-17 15:57:43 +0100

    Essentails-> Writing short and concise go code

--- 1.Essentials/main.go
+++ 1.Essentials/main.go
@@ -6,11 +6,8 @@ import (
 )
 
 func main() {
+	investedAmount, years, returnRate := 1000.0, 10.0, 5.5
 
-	var investedAmount = 1000
-	var returnRate = 0.1
-	var years = 7
-
-	var futureValue = float64(investedAmount) * math.Pow(1+returnRate/100, float64(years))
+	futureValue := float64(investedAmount) * math.Pow(1+returnRate/100, float64(years))
 	fmt.Println("Future value is ", futureValue)
 }
