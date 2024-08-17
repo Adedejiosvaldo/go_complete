ae5f904d5fd5ecde14b080aac671d6ce51094c2e
adedejiosvaldo@ <adedejiosvaldo@gmail.com>
2024-08-17 16:31:47 +0100

    Essentails-> Allowing users to input their code on the terminal

--- 1.Essentials/main.go
+++ 1.Essentials/main.go
@@ -6,8 +6,22 @@ import (
 )
 
 func main() {
-	investedAmount, years, returnRate := 1000.0, 10.0, 5.5
+	const inflationRate = 0.1
+	var investedAmount float64
+	var years, returnRate float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investedAmount)
+
+	fmt.Print("Amount of Years it will be invested for : ")
+	fmt.Scan(&years)
+
+	fmt.Print("What is the return rate: ")
+	fmt.Scan(&returnRate)
 
 	futureValue := float64(investedAmount) * math.Pow(1+returnRate/100, float64(years))
+
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Println("Future value is ", futureValue)
+	fmt.Println("Future Real value is ", futureRealValue)
 }
